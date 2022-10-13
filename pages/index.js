4c3423ca5649fb776207db475f7dcaeb6b0fd93b
jjroush <jacobroushmail@gmail.com>
2022-10-13 07:36:05 -0500

    scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ImageGallery from "react-image-gallery";
 const images = [
   {
@@ -21,6 +21,7 @@ const images = [
 
 export default function Index() {
   const [isCopied, setIsCopied] = useState(false);
+  const elementRef = useRef(null);
 
   const onEmailCopyClick = () => {
     navigator.clipboard.writeText("macro@roush.io");
@@ -48,13 +49,37 @@ export default function Index() {
     };
   }, []);
 
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleFooterScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleFooterScroll);
+      window.removeEventListener('scroll', handleFooterScroll);
+    };
+  }, [])
+
+  const handleFooterScroll = event => {
+    console.log('scrollTop: ', window.scrollY);
+    console.log('videoheight', elementRef.current?.clientHeight)
+    console.log('duration', elementRef.current.duration)
+    console.log('time', (window.scrollY * elementRef.current.duration) / elementRef.current?.clientHeight)
+
+    elementRef.current.currentTime = (window.scrollY * elementRef.current.duration) / (elementRef.current?.clientHeight / 3);
+  };
+
   return (
     <>
+      <div className="video-container-2">
+        <video className="video-header-2" ref={elementRef} muted playsInline>
+          <source src="/website-scroll_1.mp4" type="video/mp4" />
+        </video>
+      </div>
       <div className="mobile-header-container">
         <img className="logo-mobile" src="/macro-logo.svg"></img>
       </div>
       <div className="video-container">
-        <video className="video-header" muted playsInline autoPlay loop>
+        <video className="video-header"  muted playsInline autoPlay loop>
           <source src="/desktop-header-3mg.mp4" type="video/mp4" />
         </video>
       </div>
@@ -84,45 +109,58 @@ export default function Index() {
           </div>
         </div>
         <ImageGallery showPlayButton={false} items={images} lazyload={true} />
-        <div className="text-block key-info">
-          <h3 className="preheader">The Nitty Gritty</h3>
-          <h1 className="header">Let's Talk Details</h1>
-          <div className="about-group">
-            <div className="about-item">
-              <img className="logo-mobile" src="./Macro_Money.svg"></img>
-              <h1 className="details-header">120</h1>
-              <p className="details-type">dollars</p>
-            </div>
-            <div className="about-group-center">
-              <img className="logo-mobile" src="./Macro_Camera.svg"></img>
-              <h1 className="details-header">5</h1>
-              <p className="details-type">photos</p>
-            </div>
-            <div className="about-item">
-              <img className="logo-mobile" src="./Macro_Clock.svg"></img>
-              <h1 className="details-header">1</h1>
-              <p className="details-type">week</p>
-            </div>
+        {/*<div className="text-block key-info">*/}
+        {/*  <h3 className="preheader">The Nitty Gritty</h3>*/}
+        {/*  <h1 className="header">Let's Talk Details</h1>*/}
+        {/*  <div className="about-group">*/}
+        {/*    <div className="about-item">*/}
+        {/*      <img className="logo-mobile" src="./Macro_Money.svg"></img>*/}
+        {/*      <h1 className="details-header">120</h1>*/}
+        {/*      <p className="details-type">dollars</p>*/}
+        {/*    </div>*/}
+        {/*    <div className="about-group-center">*/}
+        {/*      <img className="logo-mobile" src="./Macro_Camera.svg"></img>*/}
+        {/*      <h1 className="details-header">5</h1>*/}
+        {/*      <p className="details-type">photos</p>*/}
+        {/*    </div>*/}
+        {/*    <div className="about-item">*/}
+        {/*      <img className="logo-mobile" src="./Macro_Clock.svg"></img>*/}
+        {/*      <h1 className="details-header">1</h1>*/}
+        {/*      <p className="details-type">week</p>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*  <p className="paragraph">*/}
+        {/*    A basic package includes 5 high-resolution photos and*/}
+        {/*    post-production delivered to your inbox within 3 business days.*/}
+        {/*  </p>*/}
+        {/*  <p className="paragraph">*/}
+        {/*    Each project has it's own needs. Whether you want video or more*/}
+        {/*    images, I am happy to get you a quote.*/}
+        {/*  </p>*/}
+        {/*  <h3 className="preheader">What's Next?</h3>*/}
+        {/*  <h1 className="header">Steps</h1>*/}
+        {/*  <ol className="list">*/}
+        {/*    <li>Contact me to get a quote.</li>*/}
+        {/*    <li>Ship your product out.</li>*/}
+        {/*    <li>Photos will be digitally delivered in a week.</li>*/}
+        {/*  </ol>*/}
+        {/*</div>*/}
+        <div className="two-column">
+          <div>
+            <h3 className="newpreheader">Making your products</h3>
+            <h1 className={"header"}>{"What's Next?"}</h1>
           </div>
-          <p className="paragraph">
-            A basic package includes 5 high-resolution photos and
-            post-production delivered to your inbox within 3 business days.
-          </p>
-          <p className="paragraph">
-            Each project has it's own needs. Whether you want video or more
-            images, I am happy to get you a quote.
-          </p>
-          <h3 className="preheader">What's Next?</h3>
-          <h1 className="header">Steps</h1>
-          <ol className="list">
-            <li>Contact me to get a quote.</li>
-            <li>Ship your product out.</li>
-            <li>Photos will be digitally delivered in a week.</li>
-          </ol>
+          <div>{"Steps"}</div>
         </div>
         <div className="text-block">
           <h3 className="preheader">Let's Talk</h3>
-          <h1 className="header">Reach Out To Me</h1>
+          <div>
+            <h1 className="header-number">01</h1>
+            <h1 className="header-number header">Quote</h1>
+            <p>
+              Contact Macro for a custom quote. Macro offers custom photo and video solutions based on your needs. Choose from a
+            </p>
+          </div>
         </div>
         <a href="mailto:macro@roush.io" onClick={() => onEmailLinkClick()}>
           macro@roush.io
@@ -136,6 +174,8 @@ export default function Index() {
           {isCopied ? "✅" : "📋"}
         </button>
       </div>
+      <div className="spacer"></div>
+
     </>
   );
 };
