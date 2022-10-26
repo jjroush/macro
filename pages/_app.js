1a11e068aea8fe78131fa878cb424cf095a2b371
jjroush <jacobroushmail@gmail.com>
2022-10-25 22:48:42 -0500

    setup footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+
 import '../styles/global.css'
 import '../styles/image-gallery.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+
+    Fathom.load('XRYUVLYQ', {
+      includedDomains: ['macro.roush.io'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />
 }
 
